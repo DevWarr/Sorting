9939cfa70a993ff50ee6120c2b22e1bbbf1b8596
Devin Warrick <dev.warr@gmail.com>
2020-04-13 17:01:43 -0500

    Completes merge_sort using recursion

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,7 +3,33 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+
+    # m = merged_arr index
+    # b = arrA index
+    # b = arrB index
+    m, a, b, = 0, 0, 0
+    while a < len(arrA) and b < len(arrB):
+        # Move through both halves, placing the smallest
+        # number inside the merged array
+        if arrA[a] < arrB[b]:
+            merged_arr[m] = arrA[a]
+            a += 1
+        else:
+            merged_arr[m] = arrB[b]
+            b += 1
+        m += 1
+
+    # Leftover values in either array? add them in '' '
+    if a < len(arrA):
+        for f in range(a, len(arrA)):
+            merged_arr[m] = arrA[f]
+            m += 1
+
+    if b < len(arrB):
+        for f in range(b, len(arrB)):
+            merged_arr[m] = arrB[f]
+            m += 1
+
     return merged_arr
 
 
@@ -11,7 +37,26 @@ def merge( arrA, arrB ):
 def merge_sort( arr ):
     # TO-DO
 
-    return arr
+    # Slice the array into two arrays
+    half = int(len(arr)/2)
+
+    arrA = [0] * half # proper length of array
+    for i in range(0, half):
+        arrA[i] = arr[i]
+
+    arrB = [0] * (len(arr)-half) # proper length of array
+    b = 0
+    for i in range(half, len(arr)):
+        arrB[b] = arr[i]
+        b += 1
+
+    # If either array has a length greater than 1, recurse!
+    if len(arrA) > 1:
+        arrA = merge_sort(arrA)
+    if len(arrB) > 1:
+        arrB = merge_sort(arrB)
+
+    return merge(arrA, arrB)
 
 
 # STRETCH: implement an in-place merge sort algorithm
